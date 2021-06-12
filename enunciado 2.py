b559c54b7c84c93f1b5d021f86024714f60a217f
Luyinq <85760533+Luyinq@users.noreply.github.com>
2021-06-11 23:14:42 -0400

    Update enunciado 2.py
    v2

--- enunciado 2.py
+++ enunciado 2.py
@@ -1,20 +1,11 @@
 #2
 
-nombres=["Paula", "German", "Pedro", "Esteban", "Paula", "Ricardo", "Nicole", "Francisca", "Pedro", "Pedro", "Ricardo"]
-apellidos=["Suarez", "Sanchez", "Ruiz", "Vega", "Suarez", "Gonzales", "Gonzalez", "Gonzalez"]
+nombres=["Pablo", "Pedro", "Luis", "Ana", "Lisa", "Carmen", "Jorge", "Victor", "Jorge"]
+nombres2=["Jorge", "Lisa", "Pedrinho", "Ana", "Juan", "Diego", "Victor", "Jorge"]
 repetidos=[]
-duplicado=0
 
 for x in range(len(nombres)):
-    duplicado=x+1
-    for y in range(duplicado, len(nombres)):
-        if nombres[x]==nombres[y] and nombres[x] not in repetidos:
-            repetidos.append(nombres[x])
-duplicado=0
-for x in range(len(apellidos)):
-    duplicado=x+1
-    for y in range(duplicado, len(apellidos)):
-        if apellidos[x]==apellidos[y] and apellidos[x] not in repetidos:
-            repetidos.append(apellidos[x])
-
+    for y in range(len(nombres2)):
+        if nombres[x]==nombres2[y] and nombres[x] not in repetidos:
+            repetidos.append(nombres[x])            
 print(repetidos)
