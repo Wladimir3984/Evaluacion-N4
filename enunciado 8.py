011ebdc9b36d3718907d351db5df17c28487aae6
Luyinq <85760533+Luyinq@users.noreply.github.com>
2021-06-11 21:40:46 -0400

    enunciado 8

---
+++ enunciado 8.py
@@ -0,0 +1,17 @@
+while True:
+    palabra=input("Ingrese una palabra=> ")
+    palabra2 = "" #sumador de letras al revés
+    contador=0 #contador de letras
+    if palabra.replace(" ", "").isalpha(): #omitir espacios y validar que sean solo letras
+        for i in palabra.replace(" ", ""): #recorrer palabra omitiendo espacios
+            contador+=1 #contar letras
+            palabra2= i + palabra2 #añadir letra en reversa
+        
+        if palabra.replace(" ", "") == palabra2: #comparar palabras sin espacios
+            print(f"{palabra} es un palíndromo. Tiene {contador} letras") #resultado => palabra normal y la suma de únicamente sus letras
+            break
+        else:
+            print(f"{palabra} no es un palíndromo. Tiene {contador} letras")
+            break
+    else:
+        print("Ingrese solo letras.")
